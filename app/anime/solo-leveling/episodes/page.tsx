5e55f798357ba750a8115b351069f18c1847e8a5
tasib945537-ux <tasib945537@gmail.com>
2026-09-20 15:07:07 +0600

    Replace stale EpisodesPage import with WatchPlayer route

--- app/anime/solo-leveling/episodes/page.tsx
+++ app/anime/solo-leveling/episodes/page.tsx
@@ -1,8 +1,7 @@
+'use client'
+
 import WatchPlayer from '../../../components/watch-player'
 
-const SOLO_LEVELING_MAL_ID = '16498'
-
-// Solo Leveling (MAL ID 16498) uses the existing watch player for episode playback.
 export default function AnimeEpisodesPage() {
-  return <WatchPlayer malId={SOLO_LEVELING_MAL_ID} />
+  return <WatchPlayer malId="16498" />
 }
